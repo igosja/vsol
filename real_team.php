3ffcabaa58e4dc07557eea34b84db2ee68fcd644
Igor Boyko <igor.boyko@hotelfriend.co>
2018-05-31 17:10:54 +0300

    real team

--- real_team.php
+++ real_team.php
@@ -2,8 +2,7 @@
 
 include(__DIR__ . '/simplehtmldom_1_5/simple_html_dom.php');
 
-if (isset($_POST['html']))
-{
+if (isset($_POST['html'])) {
     $html = $_POST['html'];
     $html = str_get_html($html);
     $innertext = $html->innertext;
@@ -19,6 +18,8 @@ if (isset($_POST['html']))
         $teamlink = explode(',', $teamlink);
         $teamlink = $teamlink[1];
         $teamlink = str_replace('"', '', $teamlink);
+        $teamlink = str_replace('en//', 'en/z/', $teamlink);
+        $teamlink = str_replace('http://', 'https://', $teamlink);
         $teamUrl[] = $teamlink;
     }
 
@@ -34,9 +35,12 @@ if (isset($_POST['html']))
     $playerArray = array();
     foreach ($players as $player) {
         $player = explode(', ', $player);
+        $link = str_replace('"', '', $player[7]);
+        $link = str_replace('en//', 'en/z/', $link);
+        $link = str_replace('http://', 'https://', $link);
         $playerArray[] = array(
             'name' => str_replace('"', '', $player[8]),
-            'link' => str_replace('"', '', $player[7]),
+            'link' => $link,
             'country' => $player[4],
             'lineup' => $player[9],
             'p_gk' => $player[10],
@@ -45,9 +49,54 @@ if (isset($_POST['html']))
             'p_for' => $player[18],
         );
     }
+
+
+    $playerTmArray = array();
+
+
+    foreach ($teamUrl as $lineup => $url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $html = curl_exec($ch);
+        curl_close($ch);
+        $html = str_get_html($html);
+        $table = $html->find('.items', 0);
+        if ($table) {
+            $tableTr1 = $table->find('tr[class=odd]');
+            $tableTr2 = $table->find('tr[class=even]');
+            $tableTr = array_merge($tableTr1, $tableTr2);
+            foreach ($tableTr as $tr) {
+                $tdPosition = $tr->find('td', 0);
+                $aPlayer = $tr->find('td', 1)->find('table', 0)->find('tr', 0)->find('td', 1)->find('a', 0);
+                $link = $aPlayer->href;
+                $link = explode('/', $link);
+                $link = end($link);
+                $imgCountry = $tr->find('td', 6)->find('img', 0);
+                $playerTmArray[] = array(
+                    'name' => $aPlayer->innertext,
+                    'link' => 'http://www.transfermarkt.co.uk/en//profil/spieler/' . $link,
+                    'country' => $imgCountry->title,
+                    'lineup' => $lineup + 1,
+                    'position' => $tdPosition->title,
+                );
+            }
+        }
+    }
+
+    foreach ($playerTmArray as $keyTm => $playerTm) {
+        foreach ($playerArray as $key => $player) {
+            if ($playerTm['name'] == $player['name']) {
+                unset($playerTmArray[$keyTm]);
+                unset($playerArray[$key]);
+            }
+        }
+    }
+
     print '<pre>';
-    print_r($teamUrl);
-    print '<pre>';
+    print_r($playerTmArray);
     print_r($playerArray);
     exit;
 }
