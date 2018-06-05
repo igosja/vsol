48ad26309f826e5dc8d287ac78854d79a6055bb3
igosja <igosja@ukr.net>
2018-06-05 20:55:57 +0300

    bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<a href="/real_team.php">Реальная команда</a>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+    <script src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button
+                    type="button"
+                    class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                    aria-expanded="false"
+                    aria-controls="navbar"
+            >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">ВСОЛ</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/real_team.php">Реальные команды</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+</div>
+</body>
+</html>
--- real_team.php
+++ real_team.php
@@ -38,21 +38,22 @@ if (isset($_POST['html'])) {
         $link = str_replace('"', '', $player[7]);
         $link = str_replace('en//', 'en/z/', $link);
         $link = str_replace('http://', 'https://', $link);
-        $playerArray[] = array(
-            'name' => str_replace('"', '', $player[8]),
-            'link' => $link,
-            'country' => $player[4],
-            'lineup' => $player[9],
-            'p_gk' => $player[10],
-            'p_def' => $player[12],
-            'p_mid' => $player[15],
-            'p_for' => $player[18],
-        );
+        if ($link) {
+            $playerArray[] = array(
+                'name' => str_replace('"', '', $player[8]),
+                'link' => $link,
+                'country' => $player[4],
+                'lineup' => $player[9],
+                'p_gk' => $player[10],
+                'p_def' => $player[12],
+                'p_mid' => $player[15],
+                'p_for' => $player[18],
+            );
+        }
     }
 
 
     $playerTmArray = array();
-
 
     foreach ($teamUrl as $lineup => $url) {
         $ch = curl_init();
@@ -94,14 +95,83 @@ if (isset($_POST['html'])) {
             }
         }
     }
-
-    print '<pre>';
-    print_r($playerTmArray);
-    print_r($playerArray);
-    exit;
 }
 ?>
-<form method="post">
-    <textarea name="html"></textarea>
-    <input type="submit"/>
-</form>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/css/starter-template.css">
+    <script src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button
+                    type="button"
+                    class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                    aria-expanded="false"
+                    aria-controls="navbar"
+            >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">ВСОЛ</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/real_team.php">Реальные команды</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <div class="starter-template">
+        <h1>Реальные команды</h1>
+        <div class="row">
+            <div class="col-lg-12">
+                <?php if (isset($playerTmArray) && isset($playerArray)) { ?>
+                    <table class="table table-bordered table-hover table-responsive">
+                        <tr>
+                            <th colspan="5">Добавить игроков</th>
+                        </tr>
+                        <?php foreach ($playerTmArray as $item) { ?>
+                            <tr>
+                                <td><?= $item['name']; ?></td>
+                                <td><?= $item['link']; ?></td>
+                                <td><?= $item['country']; ?></td>
+                                <td><?= $item['position']; ?></td>
+                                <td><?= $item['lineup']; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                    <table class="table table-bordered table-hover table-responsive">
+                        <tr>
+                            <th colspan="4">Удалить игроков</th>
+                        </tr>
+                        <?php foreach ($playerArray as $item) { ?>
+                            <tr>
+                                <td><?= $item['name']; ?></td>
+                                <td><?= $item['link']; ?></td>
+                                <td><?= $item['country']; ?></td>
+                                <td><?= $item['lineup']; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                <?php } else { ?>
+                    <form method="post">
+                        <textarea name="html" class="form-control" rows="10"></textarea>
+                        <input class="btn btn-default" type="submit" value="Сверить данные"/>
+                    </form>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
